907fdaa8ab01917740ff1ecc43daa9da43b5e954
gdking137 <gdkim137@gmail.com>
2022-12-06 11:48:08 -0500

    add modal type of screen transition that covers the whole screen

--- ScreenTransitionExamples/SeguePresentViewController.swift
+++ ScreenTransitionExamples/SeguePresentViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SeguePushViewController.swift
+//  SeguePresentViewController.swift
 //  ScreenTransitionExamples
 //
 //  Created by Songhee Yim on 12/6/22.
@@ -7,16 +7,16 @@
 
 import UIKit
 
-class SeguePushViewController: UIViewController {
+class SeguePresentViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
     @IBAction func tapBackButton(_ sender: UIButton) {
-        self.navigationController?
-            .popViewController(animated: true)
-        
+        self.presentingViewController?
+            .dismiss(animated: true, completion:nil)
     }
+    
+
 }
