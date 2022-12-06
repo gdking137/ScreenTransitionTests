84ee6c5b1075461ec860fb7d8181254b9040f13e
gdking137 <gdkim137@gmail.com>
2022-12-06 17:23:05 -0500

    Present push button + back button for transitioning screen on iPhone

---
+++ ScreenTransitionExamples/CodePresentViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CodePresentViewController.swift
+//  ScreenTransitionExamples
+//
+//  Created by Songhee Yim on 12/6/22.
+//
+
+import UIKit
+
+class CodePresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+}
--- ScreenTransitionExamples/ViewController.swift
+++ ScreenTransitionExamples/ViewController.swift
@@ -20,5 +20,15 @@ class ViewController: UIViewController {
         //버튼 후 네비게이션 스택에 새로운 화면 추가
         
     }
+    
+    
+    @IBAction func tapCodePresentButton(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") else {return}
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: true, completion: nil)
+        //새로운 화면 present
+        //스토리보드에 있는 콘트롤러를 찾아서 instance화 해줘야한다
+    }
+    
 }
 
