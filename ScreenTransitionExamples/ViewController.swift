23e9a0dde277bd02ad968c37f24d293f112f8fb8
gdking137 <gdkim137@gmail.com>
2022-12-06 16:56:44 -0500

    Code push button with back button(screen transition)

--- ScreenTransitionExamples/ViewController.swift
+++ ScreenTransitionExamples/ViewController.swift
@@ -13,5 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tapCodePushButton(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else {return}
+    self.navigationController?.pushViewController(viewController, animated: true)
+        //버튼 후 네비게이션 스택에 새로운 화면 추가
+        
+    }
 }
 
