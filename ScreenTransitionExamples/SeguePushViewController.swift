9f22c728eac017f9ee5ab662d19ec619f3ad40c1
gdking137 <gdkim137@gmail.com>
2022-12-06 00:57:50 -0500

    Making screen transition smooth using controller

---
+++ ScreenTransitionExamples/SeguePushViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransitionExamples
+//
+//  Created by Songhee Yim on 12/6/22.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
